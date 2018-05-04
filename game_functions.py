879c06cd26849e9741b0e7b5c4e4dae78b25dada
Yourbee <276112700@qq.com>
2018-05-04 16:03:11 +0800

    armed ship 2.1
    armed ship 2.1
    add an alien and draw it
    
    armed ship init
    complete ship and its movement.

--- game_functions.py
+++ game_functions.py
@@ -18,6 +18,8 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings,screen,ship,bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
         
 def fire_bullet(ai_settings,screen,ship,bullets):
     """如果没达到限制那就发射一颗子弹"""
@@ -43,7 +45,7 @@ def check_events(ai_settings,screen,ship,bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
             
-def update_screen(ai_settings,screen,ship,bullets):
+def update_screen(ai_settings,screen,ship,alien,bullets):
     """更新屏幕上的图像，并切换到新屏幕"""    
     #每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -51,6 +53,7 @@ def update_screen(ai_settings,screen,ship,bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
     
 def update_bullets(bullets):
     """更新子弹位置并删除已消失的子弹"""
@@ -61,7 +64,6 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    print(len(bullets))
         
     #让最近绘制的屏幕可见        
     pygame.display.flip()
