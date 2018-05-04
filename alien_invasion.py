9b71093b83bd7c1838f1e88695df8d650535cc8c
Yourbee <276112700@qq.com>
2018-05-04 21:36:20 +0800

    armed ship 2.3
    armed ship 2.3
    move those aliens
    
    armed ship 2.2
    add a group of aliens
    armed ship 2.1
    add an alien and draw it
    armed ship init
    complete ship and its movement.

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,22 +4,20 @@ Created on Wed May  2 19:47:16 2018
 
 @author: 10443
 """
-
-import sys
-
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
-from pygame.sprite import Group
+
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width,
+                                      ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     
     #创建一艘飞船
@@ -37,7 +35,8 @@ def run_game():
         
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_bullets(bullets)                     
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings,aliens)                     
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
         
